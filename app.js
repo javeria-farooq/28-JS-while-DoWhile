7c20191dac99fd7d626336544f82e188272851db
javeria-farooq <gettingeducation6@gmail.com>
2024-01-26 16:00:22 +0500

    28 JS Assignment

---
+++ app.js
@@ -0,0 +1,49 @@
+// ab hm dkhnge k while loop ks trhn se bnta h ismn bh hm wo saare kaam krskte hn jo for
+// k loop se krte hn lkn ismn variable ki initialization loop se bahr or phle hti h.or 
+// wo variable loop ka hssa nh hta loop ka hssa wo hta h jo loop k undr ho
+
+// let a = 10
+
+// loop ka hssa h ye while. or ismn hm srf condition check krate hn round brackets k undr
+// then loop mn increment ya decr krte hn means ye k js trhn hm for k loop mn teenon kaam ek
+// sth krte hn ismn us trhn nh hta ismn initialization bahar hti h, braces mn cndtion check
+// hti h then loop mn incr ya decr hta h:
+
+// while(a >= 0) {
+//     document.write(a, '<br>')
+//     a--
+// }
+
+// ab hm dkhnge: doWhile loop. do while loop mn condition agr false h bh jaegi tb bh first
+// time t condition chlegi hi . do while loop chlane ka treeqa ye h k ismn bh initialization
+// loop se bhr upr hi hti h
+
+// let a = 10
+
+// then hm khnge k ab hmn first time jo kaam lzmi chlna h oose hm do lkhnk curly braces mn 
+// lkhnge taake iska mtlb hi ye h k hm phle cndtion check nh krwa rh hm phle oose task
+// perform krne k lye de rh hn taake agr cndition false h bh jae t ye ek mrtba chl jae agr
+// hm phle hi isko koi condition de dte t condition false hne ki wjah se wo whi pe rk jta 
+// lkn hm oose do k through kh rh hn k phle kaam kro phr cndition check krna islye phle do k
+// through kaam then while k through cndtion krwaenge or incr decr ismn do k undr hi hti h while
+//  mn nh hgi. or do hmesha while k sth hi chlta h: do while k loop mn ye hta h k hm incr ya
+// decr do mn krte hn while mn nh krte jbke while k loop mn ye hta h k incr or decr while mn hi
+// hta h qk ismn koi or loop nh hta.
+
+// do{
+//     document.write(a, '<br>')
+//     a--
+// }
+
+// while(a >= 1)
+
+
+
+
+// let a = 1
+// do{
+//     console.log(a, '<br>')
+//     a++
+// }
+
+// while(a <= 5)
